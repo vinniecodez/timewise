5f95f419eceb30cd2fc8c65ab4e3a9c8751209df
vinniecodez <vinnieketto@gmail.com>
2024-10-25 23:11:42 +0500

    05. Dynamic component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
-const navItems = ['timeline', 'activities', 'progress']
+const navItems = {
+	timeline: ClockIcon,
+	activities: ListBulletIcon,
+	progress: ChartBarIcon
+}
 </script>
 <template>
 	<header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
@@ -12,7 +16,7 @@ const navItems = ['timeline', 'activities', 'progress']
 		<a href="#" class="text-sm">
 			<div v-if="true" class="flex items-center gap-1">
 				Day complete!
-				<CheckCircleIcon class="h-7 text-green-500"/>
+				<CheckCircleIcon class="h-7 text-green-500" />
 			</div>
 			<div v-else class="flex items-center gap-1">
 				<div>Progress: <span class="font-mono">20%</span></div>
@@ -24,11 +28,9 @@ const navItems = ['timeline', 'activities', 'progress']
 	</main>
 	<nav class="sticky bottom-0 z-10 bg-white">
 		<ul class="flex items-center justify-around border-t">
-			<li v-for="page in navItems" :key="page" class="flex-1">
+			<li v-for="icon, page in navItems" :key="page" class="flex-1">
 				<a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-					<ClockIcon v-if="page === 'timeline'" class="h-6 w-6" />
-					<ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6" />
-					<ChartBarIcon v-else class="h-6 w-6" /> {{ page }}
+					<component :is="icon" class="h-6 w-6" /> {{ page }}
 				</a>
 			</li>
 		</ul>
