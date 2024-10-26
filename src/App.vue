e9d5582ebc8ea6a51e6400dfd601b5befeb0064f
vinniecodez <vinnieketto@gmail.com>
2024-10-26 22:10:32 +0500

    10. Navigate between pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup>
+import { ref } from 'vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
+import TheActivities from './pages/TheActivities.vue';
+import TheProgress from './pages/TheProgress.vue';
+import TheTimeline from './pages/TheTimeline.vue';
+
+const currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+	const hash = window.location.hash.slice(1)
+
+	if ([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
+		return hash
+	}
+
+	window.location.hash = PAGE_TIMELINE
+
+	return PAGE_TIMELINE
+}
 </script>
 
 <template>
 	<TheHeader />
 	<main class="flex flex-grow flex-col">
+		<TheTimeline v-show="currentPage === PAGE_TIMELINE" />
+		<TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+		<TheProgress v-show="currentPage === PAGE_PROGRESS" />
 	</main>
-	<TheNav />
+	<TheNav :current-page="currentPage" @navigate="currentPage = $event" />
 </template>
