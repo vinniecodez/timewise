4ade8d844836755dd943ae9fbb30219558d75b91
vinniecodez <vinnieketto@gmail.com>
2024-10-27 19:33:30 +0500

    17. BaseSelect and BaseButton components

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { XMarkIcon } from '@heroicons/vue/24/outline'
+
+import BaseSelect from './BaseSelect.vue';
 
 const props = defineProps(['timelineItem'])
 
@@ -15,24 +16,13 @@ const options = [
 	{value: 2, label: 'Reading'},
 	{value: 3, label: 'Training'}
 ]
+
+const selectedActivityId = 3
 </script>
 
 <template>
 	<li>
 		<a href="#" :class="hourLinkClasses">{{ timelineItem.hour }}:00</a>
-		<div class="flex gap-2">
-			<button 
-				class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 
-				disabled:cursor-not-allowed disabled:opacity-50"
-			>
-				<XMarkIcon class="h-8" />
-			</button>
-			<select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
-				<option selected disabled value="">Rest</option>
-				<option v-for="{ value, label } in options" :key="value" :value="value">
-					{{ label }}
-				</option>
-			</select>
-		</div>
+		<BaseSelect :selected="selectedActivityId" :options="options" placeholder="Rest" />
 	</li>
 </template>
