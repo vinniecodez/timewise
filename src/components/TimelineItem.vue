52a23f896db907d858888df70e70b8ae037823d4
vinniecodez <vinnieketto@gmail.com>
2024-10-27 19:02:43 +0500

    16. Select box

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
 const props = defineProps(['timelineItem'])
 
 const hourLinkClasses = [
@@ -7,15 +9,30 @@ const hourLinkClasses = [
 		? 'bg-purple-900 font-black text-white'
 		: 'bg-gray-100 text-gray-500'
 ]
+
+const options = [
+	{value: 1, label: 'Coding'},
+	{value: 2, label: 'Reading'},
+	{value: 3, label: 'Training'}
+]
 </script>
 
 <template>
 	<li>
-	<a 
-		href="#" 
-		:class="hourLinkClasses"
-	>
-	{{ timelineItem.hour }}:00
-	</a>
+		<a href="#" :class="hourLinkClasses">{{ timelineItem.hour }}:00</a>
+		<div class="flex gap-2">
+			<button 
+				class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 
+				disabled:cursor-not-allowed disabled:opacity-50"
+			>
+				<XMarkIcon class="h-8" />
+			</button>
+			<select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
+				<option selected disabled value="">Rest</option>
+				<option v-for="{ value, label } in options" :key="value" :value="value">
+					{{ label }}
+				</option>
+			</select>
+		</div>
 	</li>
 </template>
