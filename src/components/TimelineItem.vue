2bceaf1b12c67f87ece2cac60a2992c8a921f82d
vinniecodez <vinnieketto@gmail.com>
2024-10-29 13:17:34 +0500

    19. Prop validators

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,11 +1,15 @@
 <script setup>
 
+import { HOURS_IN_DAY } from '@/constants';
 import BaseSelect from './BaseSelect.vue';
 
 const props = defineProps({
   timelineItem: {
     required: true,
-    type: Object
+    type: Object,
+		validator({ hour }) {
+			return typeof hour === 'number' && hour >= 0 && hour < HOURS_IN_DAY
+		}
   }
 })
 
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,10 +1,16 @@
 <script setup>
 import TimelineItem from '@/components/TimelineItem.vue';
+import { HOURS_IN_DAY } from '@/constants';
 
 defineProps({
   timelineItems: {
     required: true,
-    type: Array
+    type: Array,
+		validator(timelineItems) {
+			return timelineItems.every(
+				({ hour }) => typeof hour === 'number' && hour >= 0 && hour < HOURS_IN_DAY
+			)
+		}
   }
 })
 </script>
