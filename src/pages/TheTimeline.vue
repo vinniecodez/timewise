2613cfa3a16f727e1602ef14c8ccd572cd33aa07
vinniecodez <vinnieketto@gmail.com>
2024-10-29 13:22:29 +0500

    20. Validators module

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,15 +1,13 @@
 <script setup>
 import TimelineItem from '@/components/TimelineItem.vue';
-import { HOURS_IN_DAY } from '@/constants';
+import { isTimelineItemValid } from '@/validators';
 
 defineProps({
   timelineItems: {
     required: true,
     type: Array,
 		validator(timelineItems) {
-			return timelineItems.every(
-				({ hour }) => typeof hour === 'number' && hour >= 0 && hour < HOURS_IN_DAY
-			)
+			return timelineItems.every(isTimelineItemValid)
 		}
   }
 })
