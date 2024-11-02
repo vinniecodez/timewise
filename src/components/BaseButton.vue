8484c14f10b950ab790aa491e83f6946d0e4db85
vinniecodez <vinnieketto@gmail.com>
2024-11-02 21:17:11 +0500

    30. Use normal script inside components

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,14 @@
-<script setup>
+<script>
 import { BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '@/constants';
+
+const typeClasses = {
+	[BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
+	[BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
+}
+</script>
+
+<script setup>
+
 import { isButtonTypeValid } from '@/validators';
 
 defineProps({
@@ -9,11 +18,6 @@ defineProps({
 		validator: isButtonTypeValid
 	}
 })
-
-const typeClasses = {
-	[BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
-	[BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
-}
 </script>
 
 <template>
